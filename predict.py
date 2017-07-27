6d46d193d2b68eadda48887519e1e415508d8547
Mikhail Baranov <baranov.mv@gmail.com>
2017-07-27 18:02:25 +0300

    catboost

--- predict.py
+++ predict.py
@@ -4,8 +4,9 @@ import tensorflow as tf
 import matplotlib.pyplot as plt
 import numpy as np
 from sklearn import svm
-from sklearn.metrics import accuracy_score
+from sklearn.metrics import accuracy_score, f1_score
 import xgboost as xgb
+from catboost import CatBoostClassifier
 import pandas as pd
 import sys
 from optparse import OptionParser
@@ -41,15 +42,24 @@ test_y = Y[test_ind]
 # You can experiment with many other options here, using the same .fit() and .predict()
 # methods; see http://scikit-learn.org
 # This example uses the current build of XGBoost, from https://github.com/dmlc/xgboost
-gbm = xgb.XGBClassifier(max_depth=4, n_estimators=200, learning_rate=0.003).fit(train_x, np.ravel(train_y.values))
-pred_train_y = gbm.predict(train_x)
-pred_test_y = gbm.predict(test_x)
+# gbm = xgb.XGBClassifier(max_depth=4, n_estimators=200, learning_rate=0.003).fit(train_x, np.ravel(train_y.values))
+# pred_train_y = gbm.predict(train_x)
+# pred_test_y = gbm.predict(test_x)
+
 # clf = svm.SVC(gamma=0.0005, C=100., kernel='rbf')
 # clf.fit(train_x, np.ravel(train_y.values))
 
 # pred_train_y = clf.predict(train_x)
 # pred_test_y = clf.predict(test_x)
+
+model = CatBoostClassifier(iterations=30, depth=4, learning_rate=0.01, loss_function='Logloss', verbose=True)
+model.fit(train_x, np.ravel(train_y.values), verbose=True)
+pred_train_y = model.predict(train_x)
+pred_test_y = model.predict(test_x)
+
 accuracy_train = accuracy_score(train_y, pred_train_y)
 accuracy_test = accuracy_score(test_y, pred_test_y)
+f1_test = f1_score(test_y, pred_test_y)
 print("Training set accuracy: {:.2%}".format(accuracy_train))
 print("Test set accuracy: {:.2%}".format(accuracy_test))
+print("Test set F1: {:.2%}".format(f1_test))
