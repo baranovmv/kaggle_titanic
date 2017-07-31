69306a22124efd6e59564bdca1b726387b119219
Mikhail Baranov <mikhail.baranov@axelmwd.com>
2017-07-31 21:46:00 +0300

    SKLearn cross-validation

--- predict.py
+++ predict.py
@@ -3,9 +3,11 @@
 import matplotlib.pyplot as plt
 import numpy as np
 from sklearn import svm
+from sklearn import ensemble
 from sklearn.metrics import accuracy_score, f1_score
-import xgboost as xgb
-from catboost import CatBoostClassifier
+from sklearn.model_selection import cross_val_score, ShuffleSplit
+# import xgboost as xgb
+# from catboost import CatBoostClassifier
 import pandas as pd
 import sys
 from optparse import OptionParser
@@ -19,10 +21,11 @@ FILL_MEDIAN = True
 def prep_df(X_in):
     sexnum = {'male':0, 'female':1}
     embarked = {'S':0, 'C':1, 'Q':2, np.nan: np.nan}
-    X_out = X_in.loc[:,['Pclass', 'Age', 'SibSp', 'Parch', 'Fare']]
+    X_out = X_in.loc[:,['Pclass', 'Fare']]
+    X_out['FamSize'] = X_in['SibSp'] + X_in['Parch']
     X_out['EmbarkedNum'] = X_in['Embarked'].replace(embarked)
     X_out['SexNum'] = X_in['Sex'].replace(sexnum)
-    X_out['Age'] = X_out['Age'].fillna(X_out['Age'].median() if FILL_MEDIAN else -1)
+    # X_out['Age'] = X_out['Age'].fillna(X_out['Age'].median() if FILL_MEDIAN else -1)
     X_out['EmbarkedNum'] = X_out['EmbarkedNum'].fillna(X_out['EmbarkedNum'].median() if FILL_MEDIAN else -1)
     X_out['SexNum'] = X_out['SexNum'].fillna(X_out['SexNum'].median() if FILL_MEDIAN else -1)
     X_out['Pclass'] = X_out['Pclass'].fillna(X_out['Pclass'].median() if FILL_MEDIAN else -1)
@@ -58,34 +61,21 @@ test_y = Y[test_ind]
 # You can experiment with many other options here, using the same .fit() and .predict()
 # methods; see http://scikit-learn.org
 # This example uses the current build of XGBoost, from https://github.com/dmlc/xgboost
-# gbm = xgb.XGBClassifier(max_depth=16, n_estimators=60, learning_rate=0.05).fit(train_x, np.ravel(train_y.values))
+# gbm = xgb.XGBClassifier(max_depth=3, n_estimators=400, learning_rate=0.02).fit(train_x, np.ravel(train_y.values))
 # pred_train_y = gbm.predict(train_x)
 # pred_test_y = gbm.predict(test_x)
 
-# clf = svm.SVC(gamma=0.0005, C=100., kernel='rbf')
-# clf.fit(train_x, np.ravel(train_y.values))
-
-# pred_train_y = clf.predict(train_x)
-# pred_test_y = clf.predict(test_x)
-
-model = CatBoostClassifier(iterations=120, depth=16,l2_leaf_reg=4, learning_rate=0.015, loss_function='Logloss',\
-    eval_metric='AUC', verbose=True)
-model.fit(train_x, np.ravel(train_y.values), eval_set=(test_x,np.ravel(test_y)), use_best_model=False, verbose=True)
-pred_train_y = model.predict(train_x)
-pred_test_y = model.predict(test_x)
-
-accuracy_train = accuracy_score(train_y, pred_train_y)
-accuracy_test = accuracy_score(test_y, pred_test_y)
-f1_test = f1_score(test_y, pred_test_y)
-print("Training set accuracy: {:.2%}".format(accuracy_train))
-print("Test set accuracy: {:.2%}".format(accuracy_test))
-print("Test set F1: {:.2%}".format(f1_test))
+clf = ensemble.GradientBoostingClassifier(learning_rate=0.05, n_estimators=200, max_depth=2)
+cv = ShuffleSplit(n_splits=10, test_size=0.1, random_state=0)
+scores = cross_val_score(clf, train_x, np.ravel(train_y.values), cv=cv)
+print("Accuracy: %0.2f (+/- %0.2f)" % (scores.mean(), scores.std() * 2))
 
 #######################################################################################################################
 test = pd.read_csv('test.csv', index_col=0)
 test_x = prep_df(test)
 
-pred_test_y = model.predict(test_x)
-pred_test_y_df = pd.DataFrame(data=pred_test_y, index=test.index, columns=['Survived'])
-pred_test_y_df['Survived'] = pred_test_y_df['Survived'].astype('int')
-pred_test_y_df.to_csv('submission.csv')
+# pred_test_y = gbm.predict(test_x.as_matrix())
+# pred_test_y = clf.predict(test_x.as_matrix())
+# pred_test_y_df = pd.DataFrame(data=pred_test_y, index=test.index, columns=['Survived'])
+# pred_test_y_df['Survived'] = pred_test_y_df['Survived'].astype('int')
+# pred_test_y_df.to_csv('submission.csv')
